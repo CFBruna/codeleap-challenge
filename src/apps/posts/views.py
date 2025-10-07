b0d67de9449fff78aa97146e92b538e172ff732a
Bruna <brunaads.ti@gmail.com>
2025-10-07 21:45:59 +0000

    feat(posts): Create ViewSet for CRUD operations

--- src/apps/posts/views.py
+++ src/apps/posts/views.py
@@ -1,1 +1,28 @@
-# Create your views here.
+from rest_framework import mixins, viewsets
+
+from .models import Post
+from .serializers import PostSerializer, PostUpdateSerializer
+
+
+class PostViewSet(
+    mixins.CreateModelMixin,
+    mixins.RetrieveModelMixin,
+    mixins.UpdateModelMixin,
+    mixins.DestroyModelMixin,
+    mixins.ListModelMixin,
+    viewsets.GenericViewSet,
+):
+    """
+    ViewSet to handle all CRUD operations for the Post model.
+    """
+
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
+
+    def get_serializer_class(self):
+        """
+        Return the appropriate serializer class based on the request action.
+        """
+        if self.action in ["update", "partial_update"]:
+            return PostUpdateSerializer
+        return super().get_serializer_class()
